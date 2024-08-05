39db250ee1ad4587367671f6ff3fe7734f278929
KayongoYongo <sknsammie10@gmail.com>
2024-08-05 19:38:25 +0300

    Added Gemini

--- GeminiBackend.py
+++ GeminiBackend.py
@@ -15,6 +15,8 @@ import requests
 
 # Store these pdfs in a database, user_id, 
 
+# Hope it works
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
